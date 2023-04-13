5d87be8c657b0d9009acea207d2609c5274fb1b7
haoyudong <2070343054@email.szu.edu.cn>
2023-04-13 22:36:59 +0800

    add computer

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,19 @@
-import { OrbitControls } from '@react-three/drei'
+import { useGLTF, OrbitControls } from '@react-three/drei'
 import React from 'react'
-
 export default function Experience() {
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
     return (
         <>
+            <color args={['#241a1a']} attach="background" />
             <OrbitControls makeDefault />
             <mesh>
                 <boxGeometry />
                 <meshNormalMaterial />
+                <primitive object={computer.scene} />
             </mesh>
+
         </>
     )
 }
+
+
