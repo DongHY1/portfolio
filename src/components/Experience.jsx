cf88c2434801babf9e7ff7927f3a899a2aa9bf74
haoyudong <2070343054@email.szu.edu.cn>
2023-04-13 22:44:56 +0800

    add float

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { useGLTF, OrbitControls } from '@react-three/drei'
+import { Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
 import React from 'react'
 export default function Experience() {
     const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
@@ -6,12 +6,13 @@ export default function Experience() {
         <>
             <color args={['#241a1a']} attach="background" />
             <OrbitControls makeDefault />
+            <Environment preset="city" />
             <mesh>
-                <boxGeometry />
                 <meshNormalMaterial />
-                <primitive object={computer.scene} />
+                <Float rotationIntensity={0.4}>
+                    <primitive object={computer.scene} position-y={-1.2} />
+                </Float>
             </mesh>
-
         </>
     )
 }
