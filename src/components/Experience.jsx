a829b4d2541c6a6e3165cc34f824d4ef29e5b4b8
haoyudong <2070343054@email.szu.edu.cn>
2023-04-13 22:50:41 +0800

    fix rotation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,18 @@
-import { Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import { PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
 import React from 'react'
 export default function Experience() {
     const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
     return (
         <>
             <color args={['#241a1a']} attach="background" />
-            <OrbitControls makeDefault />
+
             <Environment preset="city" />
             <mesh>
-                <meshNormalMaterial />
-                <Float rotationIntensity={0.4}>
-                    <primitive object={computer.scene} position-y={-1.2} />
-                </Float>
+                <PresentationControls global>
+                    <Float rotationIntensity={0.4}>
+                        <primitive object={computer.scene} position-y={-1.2} />
+                    </Float>
+                </PresentationControls>
             </mesh>
         </>
     )
